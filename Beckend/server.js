70f9c419a95e2a00fe07f27cdb4a406fe03de228
bernardogkarsten <gomez.karsten@gmail.com>
2025-06-17 11:31:31 -0300

    primeiro update

---
+++ Beckend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const userRoutes = require('./src/routes/userRoutes');
+const pedidoRoutes = require('./src/routes/pedidoRoutes');
+const errorHandler = require('./src/middlewares/errorHandler');
+const path = require('path');
+require('dotenv').config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Expor pasta uploads
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// Rotas
+app.use('/api', userRoutes);
+app.use('/api', pedidoRoutes);
+
+// Middleware de erro
+app.use(errorHandler);
+
+// Iniciar servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
